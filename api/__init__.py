b427098203744585b022dd71937fd43aa3f3862c
Srinjan Ghosh <srinjanghosh11@gmail.com>
2019-10-13 22:57:41 +0530

    sqlite to postgres

--- api/__init__.py
+++ api/__init__.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ifkgqahuxzyvjb:fb3d75cc5ee7ba82a81f8282404ef5a08ade7b2870fb7b92c0eee8c37a2fff90@ec2-54-83-55-125.compute-1.amazonaws.com:5432/dcac5vga7nf0fc'
 
     db.init_app(app)
 
